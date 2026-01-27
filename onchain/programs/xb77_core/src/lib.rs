35fd75037979897df6e5e9527f7a3c9bffa28edc
dzkinha <195769325+dzkinha@users.noreply.github.com>
2026-01-27 18:49:39 +0000

    feat: add devnet init script and sync program IDs for deployment

--- onchain/programs/xb77_core/src/lib.rs
+++ onchain/programs/xb77_core/src/lib.rs
@@ -14,5 +14,8 @@ pub mod state;
 #[cfg(not(feature = "no-entrypoint"))]
 use processor::process_instruction;
 
+use solana_program::declare_id;
+declare_id!("FpWZN1FB9yMfip3vYQhsZhgT4fCB3US9BqAv5kh5uDxv");
+
 #[cfg(not(feature = "no-entrypoint"))]
 entrypoint!(process_instruction);
